504940b6bd7cc2b1286c2ce93bab9d02abb91bd8
koyavogui <micodium3@gmail.com>
2020-10-06 14:57:02 +0000

    firt commit

---
+++ include/script.php
@@ -0,0 +1,2 @@
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js" integrity="sha512-YSdqvJoZr83hj76AIVdOcvLWYMWzy6sJyIMic2aQz5kh2bPTd9dzY3NtdeEAzPp/PhgZqr4aJObB3ym/vsItMg==" crossorigin="anonymous"></script>
---
+++ squelette.php
@@ -0,0 +1,16 @@
+<?php
+     require 'database/database.php' 
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require 'include/head.php' ?>
+    <title>Akwaba</title>
+</head>
+<body>
+    
+
+    <?php require 'include/script.php' ?>
+</body>
+</html>
